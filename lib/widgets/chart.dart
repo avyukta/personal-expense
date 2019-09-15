44b3446f1d1cdad8c0c6cf9b23eb18861ce8cc4e
Shekhar Suman <shekhar.s@inspiria.edu.in>
2019-09-15 23:22:40 +0530

    designed chart card

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -4,8 +4,12 @@ class Chart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 6,
       margin: EdgeInsets.all(20),
       color: Theme.of(context).primaryColor,
+      child: Row(
+        children: <Widget>[Text('chart to be added.')],
+      ),
     );
   }
 }
