30b0e77bc4e622f29faa176cfef7e7bedf0082f6
Shekhar Suman <shekhar.s@inspiria.edu.in>
2019-09-15 23:19:37 +0530

    added image for no transaction

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(20),
+      color: Theme.of(context).primaryColor,
+    );
+  }
+}
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,50 +10,74 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      height: 300,
+      child: transaction.isEmpty
+          ? Column(
               children: <Widget>[
+                Text(
+                  "No Transaction Done yet!",
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
                 Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Theme.of(context).primaryColor, width: 2),
-                    //color: Theme.of(context).primaryColor,
-                  ),
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "\$${transaction[index].amount.toStringAsFixed(2)}",
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
+                  child: Image.asset(
+                    'assets/images/images.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      child: Text(transaction[index].title,
-                          style: Theme.of(context).textTheme.title),
-                    ),
-                    Container(
-                      child: Text(
-                        DateFormat("d-M-y E").format(transaction[index].date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    )
-                  ],
-                )
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                    'To see the transaction list, \n Please, add some transaction.')
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).primaryColor, width: 2),
+                          //color: Theme.of(context).primaryColor,
+                        ),
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "\$${transaction[index].amount.toStringAsFixed(2)}",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Text(transaction[index].title,
+                                style: Theme.of(context).textTheme.title),
+                          ),
+                          Container(
+                            child: Text(
+                              DateFormat("d-M-y E")
+                                  .format(transaction[index].date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: transaction.length,
             ),
-          );
-        },
-        itemCount: transaction.length,
-      ),
     );
   }
 }
